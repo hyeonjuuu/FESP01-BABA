e257d83b4191dacc90bf94e02f985d5c074d76f3
bomlang <bomlang4211@gmail.com>
2023-12-05 16:02:48 +0900

    :wrench: Vite.config.ts 절대경로설정 플러그인 추가
    tsconfigPaths플러그인을 추가하여 절대경로를 설정하였습니다.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from 'path'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
