fbb1d108f8811349936ba85f946e23a717c9849b
haha41 <thgml500@gmail.com>
2023-12-06 14:39:11 +0900

    🎨 [Design] Header CSS 완성

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,9 +1,11 @@
+import Header from '@/layout/Header'
 import { Outlet } from 'react-router-dom'
 
 export default function RootLayout() {
   return (
     <>
       <main>
+        <Header />
         <Outlet />
       </main>
     </>
