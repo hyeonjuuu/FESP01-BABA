da86ef44aef52306c79ecb818a345ea148e609fb
haha41 <thgml500@gmail.com>
2023-12-06 16:02:41 +0900

    🎨 [Design] Button 컴포넌트 CSS 완성

---
+++ src/components/Button.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface ButtonProps {
+  bgColor?: string
+  color?: string
+  text?: string
+}
+
+const BtnButton = styled.button<ButtonProps>`
+  width: 310px;
+  height: 48px;
+  margin-bottom: 20px;
+  background-color: ${props => props.bgColor};
+  color: ${props => props.color};
+`
+
+function Button({ bgColor = '#303032', color = '#46F3F3', text }: ButtonProps) {
+  return (
+    <BtnButton bgColor={bgColor} color={color}>
+      {text}
+    </BtnButton>
+  )
+}
+
+export default Button
