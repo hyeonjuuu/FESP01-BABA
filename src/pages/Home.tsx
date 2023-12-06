f091d1dcc3b697844a6375634afc75018ec8d6a0
haha41 <thgml500@gmail.com>
2023-12-06 16:50:48 +0900

    🎨 [Design] Home 페이지 CSS 완성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,29 @@
+import Button from '@/components/Button'
 import React from 'react'
+import styled from 'styled-components'
 
 function Home() {
-  return <div>Home</div>
+  const HomeDivWrapper = styled.div`
+    width: 390px;
+    margin: auto;
+  `
+
+  const ButtonDivWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `
+
+  return (
+    <HomeDivWrapper>
+      <h1>로고</h1>
+      <ButtonDivWrapper>
+        <Button text="로그인" />
+        <Button bgColor="#F8F8F8" color="#303032" text="회원가입" />
+      </ButtonDivWrapper>
+    </HomeDivWrapper>
+  )
 }
 
 export default Home
