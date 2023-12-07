e6f087b48411cd99820dc2396e99b5f57d822629
hyeonjuuu <janghyeonjuu@gmail.com>
2023-12-07 11:25:54 +0900

    🎨 [Design] 메인 페이지 마크업 및 스타일링

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
+import CategoryComponent from './../components/CategoryComponent'
+import RecommendContentsSection from '@/layout/RecommendContentsSection'
+import FeedComponent from '@/components/FeedComponent'
 
 function Home() {
-  return <div>Home</div>
+  return (
+    <>
+      <MainPageTitle aria-label="메인페이지">메인 페이지</MainPageTitle>
+      <CategoryComponent />
+      <RecommendContentsSection />
+      <FeedComponent />
+      <FeedComponent />
+    </>
+  )
 }
 
 export default Home
+
+const MainPageTitle = styled.h1`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border-width: 0;
+`
---
+++ src/style/GlobalStyle.tsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+
+body, #root {
+  font-family: GmarketSans;
+    color: #303032;
+    margin: 0;
+    padding: 0;
+    width: 390px;
+    margin-left : auto;
+    margin-right: auto;
+  }
+
+
+
+`
+
+export default GlobalStyle
