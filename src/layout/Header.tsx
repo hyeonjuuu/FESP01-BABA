2785c89ad084f2008f1d7f6cb2ec684f4fcf7ad6
hyeonjuuu <janghyeonjuu@gmail.com>
2023-12-08 13:43:32 +0900

    🎨 [Design] 다크모드 라이트 모드 아이콘 적용

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,20 +2,13 @@ import styled from 'styled-components'
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import DarkModeToggle from '@/components/DarkModeToggle'
-import darkmode from '@/assets/icon/Darkmode.svg'
-import lightmode from '@/assets/icon/Lightmode.svg'
 
-interface SizeProps {
-  size?: string
-}
 function Header() {
   return (
     <HeaderDiv>
       <HeaderDiv>
         <ArrowDiv>
           <FontAwesomeIcon icon={faAngleLeft} />
-          <ScreenMode src={darkmode} alt="" size="18px" />
-          <ScreenMode src={lightmode} alt="" size="20px" />
         </ArrowDiv>
         <DarkModeToggle />
       </HeaderDiv>
@@ -37,8 +30,4 @@ const ArrowDiv = styled.div`
   align-items: center;
   padding: 10px;
   border-bottom: 2px solid black;
-`
-const ScreenMode = styled.img<SizeProps>`
-  width: ${({ size }) => size};
-  height: ${({ size }) => size};
 `
