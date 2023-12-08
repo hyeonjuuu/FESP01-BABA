778f852caf4900c99fcf2ba7439e9c0e0c581925
hyeonjuuu <janghyeonjuu@gmail.com>
2023-12-08 14:01:11 +0900

    🎨[Design] 다크모드 아이콘 변경

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -3,23 +3,37 @@ import styled from 'styled-components'
 import darkModeIcon from '@/assets/icon/Darkmode.svg'
 import lightModeIcon from '@/assets/icon/Lightmode.svg'
 
-interface SizeProps {
+interface ButtonProps {
   size?: string
+  darkMode?: boolean
 }
 
 function DarkModeToggle() {
   const { darkMode, toggleDarkMode } = useThemeStore()
 
   return (
-    <button onClick={toggleDarkMode}>
+    <ModeButton onClick={toggleDarkMode}>
       <ScreenMode src={darkMode ? lightModeIcon : darkModeIcon} size="22px" />
-    </button>
+    </ModeButton>
   )
 }
 
 export default DarkModeToggle
 
-const ScreenMode = styled.img<SizeProps>`
+const ModeButton = styled.button`
+  box-sizing: border-box;
+  border: none;
+  outline: none;
+  background-color: inherit;
+  cursor: pointer;
+
+  align-self: flex-start;
+  display: flex;
+  padding: 0;
+`
+
+const ScreenMode = styled.img<ButtonProps>`
   width: 22px;
   height: 22px;
+  /* color: ${({ darkMode }) => (darkMode === true ? '#777777' : '#FFFFFF')}; */
 `
